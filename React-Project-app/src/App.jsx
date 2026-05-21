be8a494f0ccd865a9940cf2a5668101e29885325
Isak Forsberg <isakf999@gmail.com>
2026-05-21 15:57:36 +0200

    installed react, added 3 components

---
+++ React-Project-app/src/App.jsx
@@ -0,0 +1,17 @@
+import Project from "./Components/project"
+import Project2 from "./Components/Project2"
+import Project3 from "./Components/Project3"
+
+function App() {
+  return (
+    <>
+      <h1>My Projects</h1>
+      <Project />
+      <Project2 />
+      <Project3 />
+    </>
+  )
+ 
+}
+
+export default App
---
+++ React-Project-app/src/Components/Project.jsx
@@ -0,0 +1,16 @@
+import screenShoot from "../assets/"
+function Project() {
+    return (
+        <>
+            <h2>Project</h2>
+            <img src="" alt="Projectscreenshot" />
+            <a href="">Github Repo</a>
+            <h3>Tech used</h3>
+            <p>About the project</p>
+        </>
+    )
+ 
+ 
+}
+
+export default Project
